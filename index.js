db1c8409af9fe85f972681587dcfd3e2307e8247
GoudappaSatkhed <150431617+GoudappaSatkhed@users.noreply.github.com>
2023-12-27 15:56:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,110 @@
+//console.log("im external file")
+//document.write("hi")
+//document.writeln()"hello")
+//document.writeln("java"
+//
+//console.log(num1);
+// console.log(num);
+//var num;
+//console.log(num);
+//let num1;
+//console.log(num1);
+//const num2;
+//console.log(num2);
+//var num=10;
+//console.log(num);
+// let num1=15;
+// console.log(num1);
+// const num2=150;
+// console.log(num2);
+// var num ="hi";
+// console.log(num);
+// let num1=120;
+// console.log(num1);
+//let num1="256";
+
+// var a= Number(prompt(" 1st number"));
+// var b = Number(prompt("2nd number"));
+// var c=a+b;
+// console.log(c);
+// var num1 = 10;
+// var num2 = 0;
+// let res = num1/num2;
+// console.log(-res);
+// var num3 =15;
+// var num4 =Number( "hi");
+// console.log(num3+num4);
+// console.log(num3-num4);
+// console.log(num3+num4);
+// var num6 = 11.554
+// var num7 = 5;
+// let res1= (num7+num6);
+// console.log(res1);
+// var num6 = 11.568
+// var num7 = 5;
+//  var res8= num7.toFixed(2)
+// let res2= (num7+num6);
+// console.log(res2);
+// let num = 1234;
+// let count = 0;
+// while(no!=0);
+// {
+//     count++
+//     num=parcelInt(no/10)
+//     console.log(num)
+    
+// }
+// console.log(count)
+// let a=2;
+// let b = 20;
+// console.log(a)
+// console.log(b)
+// console.log(typeof a)
+// console.log(a/b)
+// console.l1og(parseInt(10/3));
+// //return only value int vale from number
+// console.log(parseFloat(10/3));
+// let num =1234;
+// let count = 0
+// while (num!=0)
+// {
+// count++
+// num =parceInt(num/10)
+
+
+// }
+// console.log(count)
+
+
+
+// let res =145.9845;
+// console.log(res)
+// console.log(res.toFixed(2))
+// console.log(res.toFixed(10))
+// console.log(res.toPrecision(4))
+// console.log(res.toPrecision(8))
+// console.log(res.toPrecision(6))
+// let res =14587;
+// let strRes = res.toString()
+// console.log(str.Res);
+
+let str= "rajesh is a Good boy";
+console.log(str[0])
+console.log(str[str.length-1])
+console.log(str.charAt(0))
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.charCodeAt(1));
+console.log(String.fromCharCode(65));
+console.log(str.indexOf("a"));
+console.log(str.indexOf("a",2));
+console.log(str.indexOf("z"));
+let email="mr abc@gmail.com"
+console.log(email.endsWith(".com"))
+console.log(email.endsWith("gmail"));
+console.log(email.startsWith("mr"));
+
+
+
+
+
